8bfc0ce8ebb1b43431cb5be007bf8cfdb6091995
trung058 <huutrung0508@gmail.com>
2024-11-05 21:34:52 -0800

    update

--- scripts/eachHike.js
+++ scripts/eachHike.js
@@ -19,3 +19,9 @@ function displayHikeInfo() {
         } );
 }
 displayHikeInfo();
+function saveHikeDocumentIDAndRedirect(){
+    let params = new URL(window.location.href) //get the url from the search bar
+    let ID = params.searchParams.get("docID");
+    localStorage.setItem('hikeDocID', ID);
+    window.location.href = 'review.html';
+}
--- scripts/script.js
+++ scripts/script.js
@@ -9,3 +9,19 @@ function logout() {
         // An error happened.
     });
 }
+// Add this JavaScript code to make stars clickable
+
+// Select all elements with the class name "star" and store them in the "stars" variable
+const stars = document.querySelectorAll('.star');
+
+// Iterate through each star element
+stars.forEach((star, index) => {
+    // Add a click event listener to the current star
+    star.addEventListener('click', () => {
+        // Fill in clicked star and stars before it
+        for (let i = 0; i <= index; i++) {
+            // Change the text content of stars to 'star' (filled)
+            document.getElementById(`star${i + 1}`).textContent = 'star';
+        }
+    });
+});
